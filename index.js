c59994293981a1df074b181c357cdb3a6634e524
Erica Ervin <ervin.erica29@gmail.com>
2025-08-04 21:01:20 -0400

    feat: Updated Logic & Added Function [Finished Assignment]

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
-// let order = prompt("Please list froyo order flavors here:");
-const flavors = [
-  "strawberry",
-  "strawberry",
-  "wildberry",
-  "lemon",
-  "lemon",
-  "lemon",
-  "lemon",
-  "orange",
-  "orange",
-  "blueberry",
-];
-const orderedFlavors = {};
-for (let i = 0; i < flavors.length; i++) {
-  const flavor = flavors[i];
-  // console.log(flavors[i]);
-  if (flavor in orderedFlavors) {
-    orderedFlavors[flavor] += 1;
-  } else {
-    orderedFlavors[flavor] = 1;
+// const flavors = ["strawberry", "wildberry", "lemon", "orange", "blueberry"];
+
+function orderFroyo(flavors) {
+  const orderedFlavors = {};
+  for (let i = 0; i < flavors.length; i++) {
+    const flavor = flavors[i];
+    // console.log(flavors[i]);
+    if (flavor in orderedFlavors) {
+      orderedFlavors[flavor] += 1;
+    } else {
+      orderedFlavors[flavor] = 1;
+    }
+    // console.log(orderedFlavors);
   }
-  console.log(orderedFlavors);
+  // console.log(orderedFlavors);
+  return orderedFlavors;
 }
 
-// const colors = ["red", "red", "blue", "yellow", "yellow", "yellow", "blue"];
+let order = prompt("Please list froyo order flavors separated by a comma:");
+// order.split(",").map((flavor) => flavor.trim());
+const items = order.split(",").map((flavor) => flavor.toLowerCase().trim());
+console.log(typeof order);
 
-// // red : 2, blue: 1, yellow: 3
+let result = orderFroyo(items);
+console.log(result);
 
-// // creating an object to fill with color count
-// const frequencyCounter = {};
+/*
+// const flavors = ["strawberry", "wildberry", "lemon", "orange", "blueberry"];
 
-// for (let i = 0; i < colors.length; i++) {
-//   const color = colors[i];
-//   // does the key exist in my object
-//   console.log(frequencyCounter);
-//   if (color in frequencyCounter) {
-//     // add one to the count
-//     frequencyCounter[color] += 1;
-//     // otherwise if it doesnt exist
-//   } else {
-//     // place it inside of object assign the value to 1
-//     frequencyCounter[color] = 1;
+function orderFroyo(flavors) {
+  const orderedFlavors = {};
+  for (let i = 0; i < flavors.length; i++) {
+    const flavor = flavors[i];
+    // console.log(flavors[i]);
+    if (!orderedFlavors[flavor]) {
+      orderedFlavors[flavor] = 0;
+    } 
+    orderedFlavors[flavor] += 1
+    // console.log(orderedFlavors);
+  }
+  // console.log(orderedFlavors);
+  return orderedFlavors;
+}
+
+let order = prompt("Please list froyo order flavors separated by a comma:");
+// order.split(",").map((flavor) => flavor.trim());
+const items = order.split(",").map((flavor) => flavor.toLowerCase().trim());
+console.log(typeof order);
+
+let result = orderFroyo(items);
+console.log(result);
+*/
+// function countFrequencies(arr) {
+//   const frequencies = {};
+//   for (const element of arr) {
+//     frequencies[element] = (frequencies[element] || 0) + 1;
 //   }
+//   return frequencies;
 // }
 
-console.log("freq", frequencyCounter);
+// // Example of obtaining input from a prompt (in a browser environment)
+// const inputString = prompt(
+//   "Enter array elements separated by commas (e.g., 1,2,3,1,4):"
+// );
+// if (inputString) {
+//   const inputArray = inputString.split(",").map((item) => item.trim()); // Split by comma and trim whitespace
+//   const result = countFrequencies(inputArray);
+//   console.log(result);
+// } else {
+//   console.log("No input provided.");
+// }
